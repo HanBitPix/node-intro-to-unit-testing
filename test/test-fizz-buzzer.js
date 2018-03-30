c3f7634fdccd61d8dae40a5470637982d0fdc469
HanBitPix <hanjt517@gmail.com>
2018-03-30 14:33:05 -0700

    Unit testing with Mocha and Chai

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// import chai, declare expect variable
+const expect = require('chai').expect;
+
+// import adder
+const fizzBuzzer = require('../fizzBuzzer');
+
+// unit tests for our `adder` function
+describe('fizzBuzzerAnswer', function() {
+
+  // test the normal case
+  it('should be a number', function() {
+    // range of normal inputs, including
+    // notable cases like negative answers
+    const normalCases = [
+      {a: 15, expected: 'fizz-buzz'},
+      {a: 5, expected: 'buzz'},
+      {a: 3, expected: 'fizz'},
+      {a: 1, expected: 1}
+    ];
+
+    // Loops through each normal case and makes sure its a number
+    normalCases.forEach(function(input) {
+      const answer = fizzBuzzer(input.a);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('should raise error if arg is not a number', function() {
+    // range of bad inputs where not both are numbers
+    const badInputs = [
+      ['a'],
+      ['1'],
+      ['false']
+    ];
+    // prove that an error is raised for bad inputs
+    badInputs.forEach(function(input) {
+      expect(function() {
+        fizzBuzzer(input[0]);
+      }).to.throw(Error);
+    });
+  });
+});
